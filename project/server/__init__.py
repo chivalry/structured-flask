ea5f5d523bffc8572e4d0477bfa43eaa7a5b73e5
Charles Ross <chivalry@mac.com>
2019-04-13 09:47:47 -0700

    Get ready for a databse

--- project/server/__init__.py
+++ project/server/__init__.py
@@ -3,14 +3,16 @@ import os
 from flask import Flask
 from flask_debugtoolbar import DebugToolbarExtension
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
 
 
 # instantiate the extensions
 toolbar = DebugToolbarExtension()
 bootstrap = Bootstrap()
+db = SQLAlchemy()
 
 
-def create_app(script_info=None):
+def create_app():
 
     # instantiate the app
     app = Flask(
@@ -20,11 +22,12 @@ def create_app(script_info=None):
     )
 
     # set config
-    app_settings = os.getenv('APP_SETTINGS', 'project.server.config.ProductionConfig')
+    app_settings = os.getenv('APP_SETTINGS', 'project.server.config.ProdConfig')
     app.config.from_object(app_settings)
 
     toolbar.init_app(app)
     bootstrap.init_app(app)
+    db.init_app(app)
 
     # register blueprints
     from project.server.main.views import main_blueprint
--- project/server/config.py
+++ project/server/config.py
@@ -1,18 +1,39 @@
 import os
 
+from dotenv import load_dotenv
+
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 
-class BaseConfig(object):
+class Config(object):
     """Base configuration."""
 
     APP_NAME = os.getenv('APP_NAME', 'Flask Skeleton')
     SECRET_KEY = os.getenv('SECRET_KEY', 'not-secret')
+    DEBUG_TB_ENABLED = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    WTF_CSRF_ENABLED = False
+    ADMINS = ['chivalry@mac.com']
+    LANGUAGES = ['en']
 
 
-class DevelopmentConfig(BaseConfig):
+class DevConfig(Config):
     """Development configuration."""
 
+    DEBUG_TB_ENABLED = True
+    DEBUG_TB_INTERCEPT_REDIRECTS = False
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(basedir, 'dev.db'))
 
-class ProductionConfig(BaseConfig):
+
+class TestConfig(Config):
+    TESTING = True
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL', 'sqlite://')
+
+
+class ProdConfig(Config):
     """Production configuration."""
+
+    WTF_CSRF_ENABLED = True
