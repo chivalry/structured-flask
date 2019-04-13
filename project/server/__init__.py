011e70a87e09a2c6dea49a24aa206bb279597865
Charles Ross <chivalry@mac.com>
2019-04-13 13:07:08 -0700

    Login working

--- project/server/__init__.py
+++ project/server/__init__.py
@@ -6,6 +6,7 @@ from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 # instantiate the extensions
 toolbar = DebugToolbarExtension()
@@ -13,6 +14,7 @@ bootstrap = Bootstrap()
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 migrate = Migrate()
+login_manager = LoginManager()
 
 
 def create_app():
@@ -33,12 +35,23 @@ def create_app():
     db.init_app(app)
     bcrypt.init_app(app)
     migrate.init_app(app, db)
+    login_manager.init_app(app)
 
     # register blueprints
     from project.server.main.views import main_blueprint
+    from project.server.user.views import user_blueprint
     app.register_blueprint(main_blueprint)
+    app.register_blueprint(user_blueprint)
 
-    from .models import User
+    # flask login
+    from project.server.models import User
+
+    login_manager.login_view = 'user.login'
+    login_manager.login_message_category = 'danger'
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.filter(User.id == int(user_id)).first()
 
     @app.shell_context_processor
     def shell_context_processor():
---
+++ project/server/user/views.py
@@ -0,0 +1,31 @@
+from flask import render_template, Blueprint, url_for, redirect, flash, request
+from flask_login import login_user, logout_user, login_required
+
+from project.server import bcrypt, db
+from project.server.models import User
+from project.server.user.forms import LoginForm
+
+user_blueprint = Blueprint('user', __name__)
+
+
+@user_blueprint.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm(request.form)
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, request.form['password']):
+            login_user(user)
+            flash('You are logged in. Welcome!', 'success')
+            return redirect(url_for('main.home'))
+        else:
+            flash('Invalid email or password.', 'danger')
+            return render_template('user/login.html', form=form)
+    return render_template('user/login.html', title='Please Login', form=form)
+
+
+@user_blueprint.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You were logged out. Bye!', 'success')
+    return redirect(url_for('main.home'))
