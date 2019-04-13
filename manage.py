22d0a75d7e360344deaa8d03f3866de787c3e7dc
Charles Ross <chivalry@mac.com>
2019-04-13 10:12:11 -0700

    Update manage.py

--- manage.py
+++ manage.py
@@ -4,6 +4,11 @@ from flask.cli import FlaskGroup
 
 from project.server import create_app
 
+__author__ = 'Charles Ross'
+__email__ = 'chivalry@mac.com'
+__copyright__ = 'Copyright 2019, Charles Ross'
+__version__ = '0.0.1'
+
 app = create_app()
 cli = FlaskGroup(create_app=create_app)
 
