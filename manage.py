bb439c6dab59464314b3fdcd1098431c0a9be42a
Charles Ross <chivalry@mac.com>
2019-04-13 08:18:43 -0700

    Add flake8 with command

--- manage.py
+++ manage.py
@@ -1,9 +1,17 @@
+import subprocess
+
 from flask.cli import FlaskGroup
 
 from project.server import create_app
 
 app = create_app()
 cli = FlaskGroup(create_app=create_app)
+
+
+@cli.command()
+def flake():
+    """Runs flake8 on the project."""
+    subprocess.run(['flake8'])
 
 if __name__ == '__main__':
     cli()
