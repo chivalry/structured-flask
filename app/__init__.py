bfba2e1ab1af0ee69b538e302a442f3d17234fd9
Charles Ross <chivalry@mac.com>
2019-04-17 15:06:43 -0700

    Final relative import change?

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
 from .server import create_app, db, bcrypt, constants
 from .server.models import User
-from .server.config import *
+from .server.config import DevConfig, TestConfig, ProdConfig
 from .server.user.forms import LoginForm
