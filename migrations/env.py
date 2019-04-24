72b4a4ec06676c38d4b38b34a479ceee13090522
Charles Ross <chivalry@mac.com>
2019-04-24 13:26:37 -0700

    Try a new migration init

--- migrations/env.py
+++ migrations/env.py
@@ -24,7 +24,6 @@ logger = logging.getLogger('alembic.env')
 from flask import current_app
 config.set_main_option('sqlalchemy.url',
                        current_app.config.get('SQLALCHEMY_DATABASE_URI'))
-print (current_app.extensions['migrate'].db)
 target_metadata = current_app.extensions['migrate'].db.metadata
 
 # other values from the config, defined by the needs of env.py,
@@ -82,7 +81,6 @@ def run_migrations_online():
         context.configure(
             connection=connection,
             target_metadata=target_metadata,
-            render_as_batch=True,
             process_revision_directives=process_revision_directives,
             **current_app.extensions['migrate'].configure_args
         )
--- migrations/versions/7bf4038f5463_.py
+++ migrations/versions/7bf4038f5463_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 9ead62ad44d6
+Revision ID: 7bf4038f5463
 Revises: 
-Create Date: 2019-04-13 12:24:02.454350
+Create Date: 2019-04-24 13:26:18.467377
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '9ead62ad44d6'
+revision = '7bf4038f5463'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -22,7 +22,7 @@ def upgrade():
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('timestamp', sa.DateTime(), nullable=True),
     sa.Column('email', sa.String(length=255), nullable=False),
-    sa.Column('password', sa.String(length=255), nullable=False),
+    sa.Column('_hash', sa.String(length=255), nullable=False),
     sa.Column('admin', sa.Boolean(), nullable=False),
     sa.PrimaryKeyConstraint('id'),
     sa.UniqueConstraint('email')
