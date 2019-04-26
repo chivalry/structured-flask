72620d415bdfa8b97e553a31173c33e24eb9c778
Charles Ross <chivalry@mac.com>
2019-04-25 17:26:37 -0700

    Fix typo

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from .create_app import create_app, db, bcrypt, mail  # noqaF401
+from .create_app import create_app, db, bcrypt, mail  # noqa F401
 from .models import User  # noqa F401
 from .config import DevConfig, TestConfig, ProdConfig  # noqa F401
 from .user import LoginForm  # noqa F401
